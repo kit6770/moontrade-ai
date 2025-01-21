47564bc1584556d179905cf5529c3d415552f818
kitchen <kitchen@sending.me>
2025-01-21 20:44:13 +0800

    Updates

---
+++ _next/static/chunks/app/layout-2cfc0763a0cdcb3f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2219:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,7338,23)),Promise.resolve().then(a.t.bind(a,347,23))},347:()=>{},7338:e=>{e.exports={style:{fontFamily:"'Montserrat', 'Montserrat Fallback'",fontStyle:"normal"},className:"__className_25a2d5"}}},e=>{var s=s=>e(e.s=s);e.O(0,[838,441,517,358],()=>s(2219)),_N_E=e.O()}]);
